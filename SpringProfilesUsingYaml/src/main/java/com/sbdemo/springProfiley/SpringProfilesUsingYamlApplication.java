ea17f0d8db54b44def4249746be5e0d8d5782a5c
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-08-27 19:15:16 +0530

    initial commit

---
+++ SpringProfilesUsingYaml/src/main/java/com/sbdemo/springProfiley/SpringProfilesUsingYamlApplication.java
@@ -0,0 +1,13 @@
+package com.sbdemo.springProfiley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilesUsingYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilesUsingYamlApplication.class, args);
+	}
+
+}
