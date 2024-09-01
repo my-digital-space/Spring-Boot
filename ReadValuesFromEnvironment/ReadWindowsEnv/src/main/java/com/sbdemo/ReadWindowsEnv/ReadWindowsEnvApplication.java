adae9d2180a2259cc83a62f94421fcb13a332224
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-09-01 19:27:01 +0530

    initial commit

---
+++ ReadValuesFromEnvironment/ReadWindowsEnv/src/main/java/com/sbdemo/ReadWindowsEnv/ReadWindowsEnvApplication.java
@@ -0,0 +1,13 @@
+package com.sbdemo.ReadWindowsEnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWindowsEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadWindowsEnvApplication.class, args);
+	}
+
+}
---
+++ ReadValuesFromEnvironment/ReadWindowsEnv/src/test/java/com/sbdemo/ReadWindowsEnv/ReadWindowsEnvApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.sbdemo.ReadWindowsEnv;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ReadWindowsEnvApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
