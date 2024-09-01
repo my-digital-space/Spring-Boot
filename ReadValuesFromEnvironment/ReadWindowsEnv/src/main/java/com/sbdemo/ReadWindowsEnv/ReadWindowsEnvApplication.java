fed05f750b6f5f3c66ac080fa1f5ec49ef5389bd
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-09-01 19:32:06 +0530

    implemented reading from environment variables

--- ReadValuesFromEnvironment/ReadWindowsEnv/src/main/java/com/sbdemo/ReadWindowsEnv/ReadWindowsEnvApplication.java
+++ ReadValuesFromEnvironment/ReadWindowsEnv/src/main/java/com/sbdemo/ReadWindowsEnv/ReadWindowsEnvApplication.java
@@ -1,13 +1,22 @@
 package com.sbdemo.ReadWindowsEnv;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReadWindowsEnvApplication {
+public class ReadWindowsEnvApplication implements CommandLineRunner {
+
+	@Value("${myEnvVar}")
+	private String myEnvVar;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadWindowsEnvApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\n\nmyEnvVar:"+myEnvVar+" \n\n");
+	}
 }
